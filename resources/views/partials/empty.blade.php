54529abf8acf9164d204b1d7a2fb7d02d02c59b7
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-05-06 01:45:07 +0200

    Add other pages view

---
+++ resources/views/partials/empty.blade.php
@@ -0,0 +1,9 @@
+<div class="empty">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1><---Content to insert here!---></h1>
+            </div>
+        </div>
+    </div>
+</div>
